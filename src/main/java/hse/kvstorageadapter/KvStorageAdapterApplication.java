4dbc2001860e70e7115522f353ea2ca9a95b8c8c
Chvanov Ilya <iliyatchvanov@yandex.ru>
2025-05-17 13:16:22 +0300

    Initial commit

---
+++ src/main/java/hse/kvstorageadapter/KvStorageAdapterApplication.java
@@ -0,0 +1,13 @@
+package hse.kvstorageadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvStorageAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvStorageAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hse/kvstorageadapter/KvStorageAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package hse.kvstorageadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvStorageAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
